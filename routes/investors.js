ad4ee7c4edd0d69fce92b93446044f884911fc16
ACER <fauziaazia171097@gmail.com>
2018-11-21 04:07:50 -0500

    add navbar and headers

--- routes/investors.js
+++ routes/investors.js
@@ -1,7 +1,10 @@
 let routes = require('express').Router()
 
 routes.get('/', function(req, res){
-    res.send("Welcome to DoItVest")
+    res.send("Welcome Investor of DoItVest")
+})
+routes.get('/signupInvestor',function(req,res){
+    res.render('signupInvestor.ejs')
 })
 
 module.exports = routes
